aa0447f1345582c1444518d17265105ff207e430
SmavaProject <katyakravchenka@yahoo.com>
2020-05-09 16:29:51 -0700

    cookies

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
 
 const app = express();
 //middleware to parse data from forms -???
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
 
 const colors = [
     'red',
@@ -17,17 +20,35 @@ const colors = [
 app.set('view engine', 'pug');
 
 app.get('/', (request, response)=>{
-    response.render('index');
+    const name = request.cookies.username;
+    if(name){
+        response.render('index', {name: name}); //or simply  {name}
+    }else{
+        response.redirect('hello');
+    }
+
 });
 app.get('/cards', (req, res)=>{
     res.render('card', {prompt: "Question ?", colors});
 });
+//read cookie
 app.get('/hello', (request, response)=>{
-    response.render('hello');
+    const name = request.cookies.username;
+    if (name) {
+        response.redirect('/');
+    }else{
+        response.render('hello');
+    }
 });
 app.post('/hello', (request, response)=>{
     console.log(request.body);
-    response.render('hello');
+    //send cookie to the browser after the form is submitted
+    response.cookie('username', request.body.username);
+    response.redirect('/');
+});
+app.post('/goodbye', (request, response)=>{
+    response.clearCookie('username');
+    response.redirect('/hello');
 });
 app.listen(3000, ()=> {
     console.log('The app is running on  localhost:3000');
